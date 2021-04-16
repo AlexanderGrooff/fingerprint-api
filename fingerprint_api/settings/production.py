259928e2ab5575efa51cc702bd1a734b6b134c32
Alexander Grooff <alexandergrooff@gmail.com>
2021-04-16 14:49:22 +0200

    Use postgres in production

--- fingerprint_api/settings/production.py
+++ fingerprint_api/settings/production.py
@@ -20,3 +20,14 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DATABASE"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT", 5432),
+    }
+}
