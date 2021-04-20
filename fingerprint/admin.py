2aeb9b01fdd74db56af9d7aefef3391718b3cbf3
Alexander Grooff <alexandergrooff@gmail.com>
2021-04-20 11:33:01 +0200

    Add diff viewer

--- fingerprint/admin.py
+++ fingerprint/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from django.db.models import JSONField
+from django.db.models.query import QuerySet
+from django.shortcuts import render
 
 from django_json_widget.widgets import JSONEditorWidget
 
 from fingerprint.models import Fingerprint
+
+
+@admin.action(description="Find difference")
+def find_diff(modeladmin, request, queryset: QuerySet[Fingerprint]):
+    if queryset:
+        diff = queryset[0].diff(*queryset)
+
+        return render(
+            request,
+            "diff.html",
+            context={"diff": sorted(diff.items()), "unique_fps": queryset.all()},
+        )
 
 
 @admin.register(Fingerprint)
@@ -22,3 +36,4 @@ class FingerprintAdmin(admin.ModelAdmin):
     formfield_overrides = {
         JSONField: {"widget": JSONEditorWidget},
     }
+    actions = [find_diff]
--- fingerprint/models.py
+++ fingerprint/models.py
@@ -1,4 +1,7 @@
 import json
+from collections import defaultdict
+from copy import copy
+from typing import List
 
 from django.db import models
 
@@ -35,4 +38,31 @@ class Fingerprint(models.Model):
         return "N/A"
 
     def __str__(self):
-        return f"<Fingerprint - {self.hash}>"
+        return self.hash
+
+    def _diff_to_fp(self, other_fp):
+        fp_diff = {}
+        for key in self.components.keys():
+            fp1_v = copy(self.components[key])
+            fp2_v = copy(other_fp.components[key])
+
+            if isinstance(fp1_v, dict) and isinstance(fp2_v, dict):
+                if "value" in fp1_v and "value" in fp2_v:
+                    fp1_v = fp1_v["value"]
+                    fp2_v = fp2_v["value"]
+
+                elif "duration" in fp1_v and "duration" in fp2_v:
+                    del fp1_v["duration"]
+                    del fp2_v["duration"]
+
+            if fp1_v != fp2_v:
+                fp_diff[key] = {self: fp1_v, other_fp: fp2_v}
+        return fp_diff
+
+    def diff(self, *other_fps: List["Fingerprint"]):
+        total_diff = defaultdict(dict)
+        for other_fp in other_fps:
+            fp_diff = self._diff_to_fp(other_fp)
+            for key in fp_diff:
+                total_diff[key].update(fp_diff[key])
+        return total_diff
