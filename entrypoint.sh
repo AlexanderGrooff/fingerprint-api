4f768305665cf9738de9624ac4372521cbeb1c38
Alexander Grooff <alexandergrooff@gmail.com>
2021-09-22 20:01:19 +0200

    Add docker-compose setup for development

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+
+echo "Waiting for postgres..."
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+    sleep 0.1
+done
+
+echo "PostgreSQL started"
+
 python manage.py collectstatic --clear --no-input
 python manage.py migrate
 gunicorn -c gunicorn.conf.py fingerprint_api.wsgi:application
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,1 @@
 bind = "0.0.0.0:3000"
-workers = 3
-accesslog = "/var/log/gunicorn/access.log"
-errorlog = "/var/log/gunicorn/error.log"
-capture_output = True
-loglevel = "debug"
